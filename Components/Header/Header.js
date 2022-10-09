0d2f7425e976c0eccc922111bf810405c3e264a5
Divy31245 <zaladivyb@gmail.com>
2022-10-09 22:43:52 +0530

    done

--- Components/Header/Header.js
+++ Components/Header/Header.js
@@ -1,12 +1,12 @@
 // import headerIcon from '../../assets/header/header-icon.svg'
 // import Image from 'next/image'
-import MenuIcon from '@mui/icons-material/Menu';
+
 
 import styles from './Header.module.css';
 const Header = () =>{
     return(
         <div className={styles.headerMain}>
-            <div className={styles.mobileicon} ><MenuIcon /></div>
+            <div className={styles.mobileicon} ></div>
             <div className={styles.icon} />
         </div>
     )
